ec3d0ac870520b610f086caadf51d8ea8a6961c3
Sai Byri <saikiran169@gmail.com>
2022-02-03 13:51:17 -0800

    added validaionerrors

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,4 +37,19 @@ router.post(
       return res.json({ message: 'success' })
   });
 
+// restoring a sessions user
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
+
 module.exports = router;
