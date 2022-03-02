1101a52d026f23d47671988244922dabf5afbc3e
Sai Byri <saikiran169@gmail.com>
2022-03-02 12:59:36 -0500

    added demo to form modal

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -20,38 +20,48 @@ function LoginForm () {
             });
     }
 
+    const demoClick = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.login({credential: 'Demo-lition', password: 'password'}))
+    }
+
     return(
-        <form className='formMainDiv' onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, errorId) => (
-                <li key={errorId}>{error}</li>
-                ))}
-            </ul>
-            <div className='formWelcomeBox'>
-                <h2 className='formWelcomeText'>Welcome to SustainaBnb</h2>
+        <div>
+            <form className='formMainDiv' onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map((error, errorId) => (
+                    <li key={errorId}>{error}</li>
+                    ))}
+                </ul>
+                <div className='formWelcomeBox'>
+                    <h2 className='formWelcomeText'>Welcome to SustainaBnb</h2>
+                </div>
+                <div>
+                    <label className='forminputs'>
+                        <input
+                            type="text"
+                            placeholder='Username or Email'
+                            value={credential}
+                            onChange={(e) => setCredential(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                </div>
+                <div className='submitButtons'>
+                    <button className='submitBtn' type='submit'>Log In</button>
+                </div>
+            </form>
+            <div className='demoUserBox'>
+                <button className='demoUserLogBtn' onClick={demoClick}>Demo User</button>
             </div>
-            <div>
-                <label className='forminputs'>
-                    <input
-                        type="text"
-                        placeholder='Username or Email'
-                        value={credential}
-                        onChange={(e) => setCredential(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-            </div>
-            <div className='submitButtons'>
-                <button className='submitBtn' type='submit'>Log In</button>
-            </div>
-        </form>
+        </div>
     );
 }
 
